4756848e54a7b414d476daf60b95596b0f847436
BatuhanKaraahmet <135848588+BatuhanKaraahmet@users.noreply.github.com>
2025-05-03 18:41:31 +0300

    Update tesla_bot.py

--- tesla_bot.py
+++ tesla_bot.py
@@ -40,16 +40,21 @@ def get_vehicle_ids():
 def main():
     print("Bot başlatıldı...")
     known = get_vehicle_ids()
+    print(f"İlk bilinen araçlar: {known}")
     send_telegram_message(f"Başladı. {len(known)} araç var.")
     while True:
+        print("1 dakika bekleniyor...")
         time.sleep(60)
         current = get_vehicle_ids()
+        print(f"Güncel araçlar: {current}")
         new = current - known
         if new:
+            print(f"Yeni araçlar bulundu: {new}")
             send_telegram_message(f"{len(new)} yeni araç eklendi! 🚗")
             known = current
         else:
             print("Yeni araç yok.")
+
 
 if __name__ == "__main__":
     main()
